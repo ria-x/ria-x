8d8da72eefb970d05dc53fd33c6794b3d6cb60b6
Ria.x <ria.x@mail.ru>
2024-01-24 15:27:28 +0300

    использование консоли2

--- massiv/Program.cs
+++ massiv/Program.cs
@@ -1,12 +1,35 @@
 ﻿//Console.WriteLine("Hello world!!");
 
-int value = 100;
-Console.WriteLine(value);
+// int value = 100;
+// Console.WriteLine(value);
 
-double d = 5.55;
-char c = '!';
-bool b = true;
+// double d = 5.55;
+// char c = '!';
+// bool b = true;
 
-Console.WriteLine(d);
-Console.WriteLine(c);
-Console.WriteLine(b);
+// Console.WriteLine(d);
+// Console.WriteLine(c);
+// Console.WriteLine(b);
+
+int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+
+long l1 = 6;
+long sum2 =l1 + i2;
+Console.WriteLine(sum2);
+
+
+double d1 = 5.5;
+double sum3 = d1 + i2;
+Console.WriteLine(sum3);
+
+
+int product1 = i1 * i2;
+long product2 = i2 * l1;
+double product3 = l1 * d1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
