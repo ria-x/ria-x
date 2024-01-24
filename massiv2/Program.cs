dab1a36917bb0c445727928b365750a436ba6fd8
Ria.x <ria.x@mail.ru>
2024-01-24 15:49:03 +0300

    сравнение переменных2

--- massiv2/Program.cs
+++ massiv2/Program.cs
@@ -1,10 +1,34 @@
-﻿int a = 5;
-int b = 7;
-if (a > b)
+﻿// int a = 5;
+// int b = 7;
+// if (a > b)
+// {
+//     Console.Write(a);
+// }
+// else
+// {
+//     Console.Write(b);
+// }
+
+int a = 4;
+int b = 6;
+int c = 7;
+int d = 4;
+int e = 9;
+int max = a;
+if (b > max)
 {
-    Console.Write(a);
+    max = b;
 }
-else
+if (c > max)
 {
-    Console.Write(b);
+    max = c;
 }
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.Write(max);
