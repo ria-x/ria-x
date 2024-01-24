96e268732723bdf3227321f0bd030b4b5c9ac918
Ria.x <ria.x@mail.ru>
2024-01-24 16:51:05 +0300

    использование остатка

--- massiv2/Program.cs
+++ massiv2/Program.cs
@@ -33,10 +33,30 @@
 // }
 // Console.Write(max);
 
-int n = 9;
-int i = 1;
-while (i <= n){
-    Console.Write(i);
-    Console.Write(' ');
-    i = i + 1;
+// int n = 9;
+// int i = 1;
+// while (i <= n){
+//     Console.Write(i);
+//     Console.Write(' ');
+//     i = i + 1;
+// }
+
+// int num = 6;
+// int num2 = num * (-1);
+// while (num2 <= num)
+// {
+//     Console.Write($"{num2} ");
+//     num2++;
+// }
+
+int num = 92;
+if (num >= 100 && num <= 999)
+{
+    int num1 = num / 100;
+    int num2 = num % 10;
+    int sum = num1 + num2;
+    Console.Write($"Sum of first and last digit in numd {num} is {sum}");
+}
+else{
+   Console.Write($"your numbs is not 3 digit");
 }
