75a05a5d17f1b6d79467ee38c86286f03946c509
Ria.x <ria.x@mail.ru>
2024-01-24 16:03:35 +0300

    использование цикла

--- massiv2/Program.cs
+++ massiv2/Program.cs
@@ -9,26 +9,34 @@
 //     Console.Write(b);
 // }
 
-int a = 4;
-int b = 6;
-int c = 7;
-int d = 4;
-int e = 9;
-int max = a;
-if (b > max)
-{
-    max = b;
+// int a = 4;
+// int b = 6;
+// int c = 7;
+// int d = 4;
+// int e = 9;
+// int max = a;
+// if (b > max)
+// {
+//     max = b;
+// }
+// if (c > max)
+// {
+//     max = c;
+// }
+// if (d > max)
+// {
+//     max = d;
+// }
+// if (e > max)
+// {
+//     max = e;
+// }
+// Console.Write(max);
+
+int n = 9;
+int i = 1;
+while (i <= n){
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
 }
-if (c > max)
-{
-    max = c;
-}
-if (d > max)
-{
-    max = d;
-}
-if (e > max)
-{
-    max = e;
-}
-Console.Write(max);
